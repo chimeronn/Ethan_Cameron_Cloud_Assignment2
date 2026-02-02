086e2f56836fcd62b0f5cbe87221d272a5a3c1be
chimeronn <111400368+chimeronn@users.noreply.github.com>
2026-02-02 03:41:08 -0600

    changed output freq

--- src/main/java/edu/utexas/cs/cs378/MainServer.java
+++ src/main/java/edu/utexas/cs/cs378/MainServer.java
@@ -40,7 +40,7 @@ public class MainServer {
 				stat.addMedallion(medallion);
 
 				count++;
-				if(count % 100000 == 0) {
+				if(count % 1000000 == 0) {
 					System.out.println("Processed " + count + " lines.");
 				}
 			}
